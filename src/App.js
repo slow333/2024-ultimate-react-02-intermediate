64f6a282da3e4e6d25902471d7eb66e09b5dd66a
kalpa <kalpa@kakao.com>
2024-02-29 17:13:38 +0900

    starter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [advice, setAdvice] = useState('')
+  const [value, setValue] = useState('');
+  const [results, setResults ] = useState([])
+  async function getAdvice() {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
+
+  function onChangeEvent(ev) {
+    ev.preventDefault();
+    setValue(ev.target.value)
+
+  }
+  async function getResults(ev) {
+    ev.preventDefault();
+    console.log(value)
+    const res = await fetch(`https://api.adviceslip.com/advice/search/${value}`);
+    if(!res.ok) return;
+    const data = await res.json();
+    if(!data) return;
+    setResults(data.slips);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello react
+      <form onSubmit={getResults}>
+        <input type='text' value={value} onChange={onChangeEvent} />
+        <button>submit</button>
+      </form>
+      <ul>
+        {results.map(result => <li key={result.id}>{result.id} : {result.advice}</li>)}
+      </ul>
+
+      <button onClick={getAdvice}>get Advice</button>
+      <p>{advice}</p>
+
     </div>
   );
 }
